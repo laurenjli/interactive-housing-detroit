3854b49e425c8585cda10798ce89ae6b601f30de
Lauren Li <laurenli@Laurens-MacBook-Pro.local>
2019-03-01 23:24:35 -0600

    updated formatting and text

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 //repo - scaffold for server to hold json
 // import Blight from './data/blight.json'
 
-//referenced; http://bl.ocks.org/d3noob/9211665
+//referenced: http://bl.ocks.org/d3noob/9211665
+//referenced: https://www.w3schools.com/howto/howto_css_loader.asp
+
 var CURR_YEAR = 15
 //load data
 Promise.all(
   [d3.json("./data/districts.geojson"),
    d3.json("./data/neighborhoods.geojson"),
    d3.json("./data/blight.json"),
-   d3.json("./data/dem.json"),
+   //d3.json("./data/dem.json"),
    d3.json("./data/sales.json")
   ])
   .then(function(data) {
@@ -21,12 +23,12 @@ Promise.all(
   .catch(error => console.warn(error))
 
 function makeMap(dataset) {
-  const [dist, nhood, blight, dem, sales] = dataset;
+  const [dist, nhood, blight, sales] = dataset;
 
-  var map = L.map('chart', { center: [42.3314, -83.0458], zoom:11}); //setView(new L.LatLng(42.3314, -83.0458));
+  var map = L.map('chart', { center: [42.3614, -83.0458], zoom:11}); //setView(new L.LatLng(42.3314, -83.0458));
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap' }).addTo(map);
   L.geoJSON(dist, {
-    "color": 'blue',
+    "color": '#31044F',
     "weight": 5,
     "opacity": 0.75,
   }).addTo(map);
@@ -34,31 +36,31 @@ function makeMap(dataset) {
   var myRenderer = L.canvas({ padding: 0.5 });
 
   //blight
-  var blight_curr = blight.filter(d => d.year == CURR_YEAR)
-
-  for (var i = 0; i < Object.keys(blight_curr).length; i += 1) {
-    L.circleMarker([blight_curr[i].lat, blight_curr[i].long], {
-      renderer: myRenderer,
-      color: 'red',
-      fillColor: 'red',
-      fillOpacity: 0.75,
-      radius: 1
-    }).addTo(map);
-  }
-
-
-  //demolitions
-  var dem_curr = dem.filter(d => d.year == CURR_YEAR)
-
-  for (var i = 0; i < Object.keys(dem_curr).length; i += 1) {
-  	L.circleMarker([dem_curr[i].lat, dem_curr[i].long], {
-    	renderer: myRenderer,
-      color: 'purple',
-      fillColor: 'purple',
-      fillOpacity: 0.75,
-      radius: 1
-    }).addTo(map);
-  }
+  // var blight_curr = blight.filter(d => d.year == CURR_YEAR)
+  //
+  // for (var i = 0; i < Object.keys(blight_curr).length; i += 1) {
+  //   L.circleMarker([blight_curr[i].lat, blight_curr[i].long], {
+  //     renderer: myRenderer,
+  //     color: '#B0EE31',
+  //     fillColor: '#B0EE31',
+  //     fillOpacity: 0.75,
+  //     radius: 1
+  //   }).addTo(map);
+  // }
+  //
+  //
+  // //demolitions
+  // var dem_curr = dem.filter(d => d.year == CURR_YEAR)
+  //
+  // for (var i = 0; i < Object.keys(dem_curr).length; i += 1) {
+  // 	L.circleMarker([dem_curr[i].lat, dem_curr[i].long], {
+  //   	renderer: myRenderer,
+  //     color: '#14E5D0',
+  //     fillColor: '#14E5D0',
+  //     fillOpacity: 0.75,
+  //     radius: 1
+  //   }).addTo(map);
+  // }
 
   //sales
   var sales_curr = sales.filter(d => d.year == CURR_YEAR)
@@ -66,9 +68,9 @@ function makeMap(dataset) {
   for (var i = 0; i < Object.keys(sales_curr).length; i += 1) {
   	L.circleMarker([sales_curr[i].lat, sales_curr[i].long], {
     	renderer: myRenderer,
-      color: 'green',
-      fillColor: 'green',
-      fillOpacity: 0.75,
+      color: '#8C82FA',
+      fillColor: '#8C82FA',
+      fillOpacity: 0.05,
       radius: 1
     }).addTo(map);
   }
