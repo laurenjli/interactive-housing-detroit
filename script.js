2a0d4ed4b0597ef3ae4737b2517fe5658b4e4d63
Lauren Li <laurenli@Laurens-MacBook-Pro.local>
2019-03-07 22:23:45 -0600

    added transitions for table

--- script.js
+++ script.js
@@ -14,20 +14,18 @@ Promise.all(
    d3.json("./data/blight.geojson"),
    d3.json("./data/dem.geojson"),
    d3.json("./data/sales.geojson"),
-   //d3.json("./data/summary.json")
+   d3.json("./data/summary.json")
   ])
   .then(function(data) {
     console.log(data)
     makeVis(data)
-    //makeDistrictDotMap(data)
-    //makeNHoodDotMap(data)
     })
   .catch(error => console.warn(error))
 
 
 function makeVis(dataset) {
-  // const summaryStats = dataset[5];
-  // makeTable(summaryStats);
+  const summaryStats = dataset[5];
+  makeTable(summaryStats);
   makeMap(dataset);
 
 }
@@ -38,43 +36,148 @@ function makeTable(summary){
 'Pct Change in Total from Previous Year', 'Average Amount',
 'Pct Change in Avg Amount from Previous Year'];
 
-  var summaryYr = summary.filter(d => d.Year == CURR_YEAR)
+  // var summaryYr = summary.filter(d => d.Year == CURR_YEAR)
+  //
+  // var title = d3.select('.table-title').append('text');
+  // title.text('Summary of 20' + CURR_YEAR);
+  //
+  // var table = d3.select('.statsTable')
+	// 				.append("table")
+	// 				.property("border","1px");
+  //
+  // var thead = table.append('thead');
+  // var	tbody = table.append('tbody');
+  //
+  // thead.append('tr')
+	// 	  .selectAll('th')
+	// 	  .data(colNames).enter()
+	// 	  .append('th')
+	// 	    .text(d => d);
+  //
+  // //create a row for each object in the data
+	// var rows = tbody.selectAll('tr')
+	//   .data(summaryYr)
+	//   .enter()
+	//   .append('tr');
+  //
+	// //create a cell in each row for each column
+	// var cells = rows.selectAll('td')
+	//   .data(function (row) {
+	//     return colNames.map(function (colNames) {
+	//       return {colNames: colNames, value: row[colNames]};
+	//     });
+	//   })
+	//   .enter()
+	//   .append('td')
+	//     .text(d => d.value);
 
-  var title = d3.select('.table-title').append('text')
-      .text('Summary of 20' + CURR_YEAR)
+  var title = d3.select('.table-title').append('text');
+  d3.selectAll('.dot-type').on("change", tableUpdate);
+  tableUpdate()
 
-  var table = d3.select('.statsTable').append('table')
-	var thead = table.append('thead')
-	var	tbody = table.append('tbody');
+  //handle checkbox year changes
+  document.getElementById("2015-box").addEventListener('click', function(event) {
+    CURR_YEAR = 15;
+    console.log(CURR_YEAR)
 
-  thead.append('tr')
-		  .selectAll('th')
-		  .data(colNames).enter()
-		  .append('th')
-		    .text(d => d);
+    d3.selectAll('.dot-type').on("change", tableUpdate);
+    tableUpdate()
+  });
 
-  //create a row for each object in the data
-	var rows = tbody.selectAll('tr')
-	  .data(summaryYr)
-	  .enter()
-	  .append('tr');
+  document.getElementById("2016-box").addEventListener('click', function(event) {
+    CURR_YEAR = 16;
+    console.log(CURR_YEAR)
 
-	// create a cell in each row for each column
-	var cells = rows.selectAll('td')
-	  .data(function (row) {
-	    return colNames.map(function (colNames) {
-	      return {colNames: colNames, value: row[colNames]};
-	    });
-	  })
-	  .enter()
-	  .append('td')
-	    .text(d => d.value);
+    d3.selectAll('.dot-type').on("change", tableUpdate);
+    tableUpdate()
+  });
+
+  document.getElementById("2017-box").addEventListener('click', function(event) {
+    CURR_YEAR = 17;
+    console.log(CURR_YEAR)
+
+    d3.selectAll('.dot-type').on("change", tableUpdate);
+    tableUpdate()
+  });
+
+  document.getElementById("2018-box").addEventListener('click', function(event) {
+    CURR_YEAR = 18;
+    console.log(CURR_YEAR)
+
+    d3.selectAll('.dot-type').on("change", tableUpdate);
+    tableUpdate()
+  });
+
+  function tableUpdate() {
+    //should change table
+    var summaryYr = summary.filter(d => d.Year == CURR_YEAR);
+    //var title = d3.select('.table-title').append('text');
+    title.text('Summary of 20' + CURR_YEAR);
+
+    d3.select('.statsTable').selectAll("*").remove();
+
+    var table = d3.select('.statsTable')
+  					.append("table")
+  					.property("border","1px");
+
+    var thead = table.append('thead');
+    var	tbody = table.append('tbody');
+
+    thead.append('tr')
+  		  .selectAll('th')
+  		  .data(colNames).enter()
+  		  .append('th')
+  		    .text(d => d);
+
+
+    //create a row for each object in the data
+  	var rows = tbody.selectAll('tr')
+  	  .data(summaryYr)
+  	  .enter()
+  	  .append('tr');
+
+  	//create a cell in each row for each column
+  	var cells = rows.selectAll('td')
+  	  .data(function (row) {
+  	    return colNames.map(function (colNames) {
+  	      return {colNames: colNames, value: row[colNames]};
+  	    });
+  	  })
+  	  .enter()
+  	  .append('td')
+  	    .text(d => d.value);
+
+    // console.log(newData)
+    // console.log('row')
+    //
+    // var newRows = tbody.selectAll('tr').data(newData)
+  	//   .enter()
+  	//   .append('tr').exit().remove();
+    //
+    // console.log('cell')
+    //
+    // var newCells = newRows.selectAll('td')
+  	//   .data(function (row) {
+  	//     return colNames.map(function (colNames) {
+  	//       return {colNames: colNames, value: row[colNames]};
+  	//     });
+  	//   })
+    //   .enter()
+  	//   .append('td')
+  	//     .text(d => d.value);
+    //
+    // //newRows.data(newData).exit().remove();
+    // newCells.exit().remove();
+
+
+  };
+
 }
 
 
 
 function makeMap(dataset) {
-  const [dist, nhood, blight, dem, sales] = dataset;
+  const [dist, nhood, blight, dem, sales, disregard] = dataset;
 
   var map = L.map('chart', { center: [42.3614, -83.0458], zoom:11}); //setView(new L.LatLng(42.3314, -83.0458));
 
@@ -134,7 +237,7 @@ function makeMap(dataset) {
     .style("opacity", 0)
     .style("z-index", "999");
 
-  //defaults  
+  //defaults
   var demCurr = dem.features.filter(d => d.properties.year == CURR_YEAR);
   plotPoints(reformat(demCurr), map, 'dem', 'Demolition', tooltip, g, svg);
 
@@ -183,7 +286,7 @@ function makeMap(dataset) {
     var demCurr = dem.features.filter(d => d.properties.year == CURR_YEAR);
     var salesCurr = sales.features.filter(d => d.properties.year == CURR_YEAR);
 
-    console.log(choices)
+    //console.log(choices)
     if(choices.includes('blight-box')){
       //plot blight
       plotBlight(reformat(blightCurr), map, 'blight', 'Blight Violation', tooltip, g, svg);
@@ -253,7 +356,7 @@ function plotPoints(dataset, map, type, title, tooltip, g, svg){
              .on("mouseover", function(d){
                d3.select(this)
                 .classed('hovered', true)
-                .transition().duration(300)
+                .transition().duration(200)
                 .attr('r', 6);
 
                tooltip.transition().duration(100).style('opacity',1)
@@ -321,7 +424,7 @@ function plotBlight(dataset, map, type, title, tooltip, g, svg){
              .on("mouseover", function(d){
                d3.select(this)
                 .classed('hovered', true)
-                .transition().duration(300)
+                .transition().duration(200)
                 .attr('r', 6);
 
                tooltip.transition().duration(100).style('opacity',1)
